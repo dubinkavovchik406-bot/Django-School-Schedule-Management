b76bc4203fd1cbc707b37cad84f82978961a6b81
Вова Дубинка <dubinkavovchik406@gmail.com>
2025-09-23 20:01:28 +0300

    add models: Subject and Teacher

--- my_project/models.py
+++ my_project/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Teacher(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Subject(models.Model):
+    name = models.CharField(max_length=100)
+    teacher = models.ForeignKey(Teacher, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.name
