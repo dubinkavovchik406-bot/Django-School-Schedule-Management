cd0d85157e76f4d881002d141863005d31b498b2
Вова Дубинка <dubinkavovchik406@gmail.com>
2025-09-23 22:05:51 +0300

    add funk delete_subject and some update funk befor

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def add_subject(name, teacher_id):
     subject, created = Subject.objects.get_or_create(name=name, teacher_id=teacher_id)
     if created is True:
         print(f"Предмет {name} успішно створений")
-    if created is False:
+    elif created is False:
         print(f"Предмет {name} вже існує")
     return subject
 
@@ -14,10 +14,17 @@ def add_teacher(name):
     teacher, created = Teacher.objects.get_or_create(name=name)
     if created is True:
         print(f"Вчитель {name} успішно створений")
-    if created is False:
+    elif created is False:
         print(f"Вчитель {name} вже існує")
-    return teacher.id
+    return teacher
+
+def delete_subject(name):
+    deleted_count, _ = Subject.objects.filter(name=name).delete()
+    if deleted_count > 0:
+        print(f"Предмет {name} успішно видалений")
+    else:
+        print(f"Предмет {name} не знайдено")
 
 
-created_teacher = add_teacher("Анна")
-add_subject("Вова", created_teacher)
+created_teacher = add_teacher("Ілля")
+add_subject("Вова", created_teacher.id)
