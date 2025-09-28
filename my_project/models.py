5e4eb15a3e2e5ee02a2ffe5d9c7ecc710c74132a
Вова Дубинка <dubinkavovchik406@gmail.com>
2025-09-28 18:22:56 +0300

    add some chenges

--- my_project/models.py
+++ my_project/models.py
@@ -1,29 +1,33 @@
 from django.db import models
 
 # Create your models here.
+class Subject(models.Model):
+    name = models.CharField(max_length=100, default="")
+    description = models.TextField(default="")
+
+    def __str__(self):
+        return f"{self.name}, {self.description}"
 
 class Teacher(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, default="")
+    surname = models.CharField(max_length=100, default="")
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.name
+        return f"{self.name}, {self.surname}"
 
-class Subject(models.Model):
-    name = models.CharField(max_length=100)
-    teacher = models.ForeignKey(Teacher, on_delete=models.DO_NOTHING)
-
-    def __str__(self):
-        return self.name
-
-class Student(models.Model):
-    name = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
 
 class Class(models.Model):
-    name = models.CharField(max_length=100)
-    student = models.ForeignKey(Student, on_delete=models.DO_NOTHING)
+    name = models.CharField(max_length=100, default="")
+    year_of_study = models.IntegerField(default=2000)
 
     def __str__(self):
-        return self.name
+        return f"{self.name}, {self.year_of_study}"
+
+class Student(models.Model):
+    name = models.CharField(max_length=100, default="")
+    surname = models.CharField(max_length=100, default="")
+    student_class = models.ForeignKey(Class, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name}, {self.surname}"
