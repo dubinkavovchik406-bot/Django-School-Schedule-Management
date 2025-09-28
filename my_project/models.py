37c044e2dec9bacdb807bc7a4e9ca85954fbcf67
Вова Дубинка <dubinkavovchik406@gmail.com>
2025-09-28 14:13:08 +0300

    add model Student

--- my_project/models.py
+++ my_project/models.py
@@ -14,3 +14,9 @@ class Subject(models.Model):
 
     def __str__(self):
         return self.name
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
